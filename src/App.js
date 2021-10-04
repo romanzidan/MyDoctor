fbcf58a372031267644e05b53a2b0ff41fef1afd
rzkzx <romanzidanramadhan@gmail.com>
2021-10-04 22:48:22 +0800

    Stack Navigator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {GetStarted} from './pages';
 import {NavigationContainer} from '@react-navigation/native';
+import Router from './router';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <GetStarted />
+      <Router />
     </NavigationContainer>
   );
 }
