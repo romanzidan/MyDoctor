b406f724452767776d5df0aa76685228dbfd8ed4
rzkzx <romanzidanramadhan@gmail.com>
2021-10-04 22:36:25 +0800

    Install React Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {GetStarted} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
-  return <GetStarted />;
+  return (
+    <NavigationContainer>
+      <GetStarted />
+    </NavigationContainer>
+  );
 }
