13040befa82b27345ca3ea7a1c263d795765da36
rzkzx <romanzidanramadhan@gmail.com>
2021-10-08 21:14:25 +0800

    Slicing Doctor Page - Rated Doctor

--- src/components/molecules/RatedDoctor/index.js
+++ src/components/molecules/RatedDoctor/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {colors, fonts} from '../../../utils';
+import {DummyDoctor3, IconStar} from '../../../assets';
 
 export default function RatedDoctor() {
   return (
-    <View>
-      <Text>RatedDoctor</Text>
+    <View style={styles.container}>
+      <Image source={DummyDoctor3} style={styles.avatar} />
+      <View style={styles.profile}>
+        <Text style={styles.name}>Ganyu Putri</Text>
+        <Text style={styles.category}>Psikiater</Text>
+      </View>
+      <View style={styles.rate}>
+        <IconStar />
+        <IconStar />
+        <IconStar />
+        <IconStar />
+        <IconStar />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 16,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    marginRight: 12,
+  },
+  rate: {
+    flexDirection: 'row',
+  },
+  profile: {flex: 1},
+  name: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+  },
+  category: {
+    fontSize: 13,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.secondary,
+    marginTop: 2,
+  },
+});
