438b4dfd43ec1d86105d10c0b96aa52b392423fe
rzkzx <romanzidanramadhan@gmail.com>
2021-10-15 06:13:24 +0800

    Setup Firebase 9 & Register

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import {initializeApp} from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyBugRpMxHzkCuvW69YEZPIHMUECEQN5QFc',
   authDomain: 'my-doctor-75ea0.firebaseapp.com',
@@ -13,6 +7,4 @@ const firebaseConfig = {
   appId: '1:718971659739:web:8580c9ea4d206bf2a573fe',
 };
 
-// Initialize Firebase
-const Firebase = initializeApp(firebaseConfig);
-export default Firebase;
+export {firebaseConfig};
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {Button, Gap, Header, Input} from '../../components';
 import {colors, useForm} from '../../utils';
+import {firebaseConfig} from '../../config';
+
+//firebase
+import {initializeApp} from 'firebase/app';
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 
 export default function Register({navigation}) {
+  //initial firebase
+  initializeApp(firebaseConfig);
+
   const [form, setForm] = useForm({
     fullName: '',
     profession: '',
@@ -14,6 +22,20 @@ export default function Register({navigation}) {
   const onContinue = () => {
     // navigation.navigate('UploadPhoto');
     console.log(form);
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, form.email, form.password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        console.log('register success', user);
+        // ...
+      })
+      .catch(error => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('error ', errorMessage);
+        // ..
+      });
   };
   return (
     <View style={styles.page}>
