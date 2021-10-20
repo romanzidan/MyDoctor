ad1ad8b8823a1f0550e189b8d32ccc10d56dd7a2
rzkzx <romanzidanramadhan@gmail.com>
2021-10-20 12:24:52 +0800

    Clean Code

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
-import {Button, Gap, Header, Input, Loading} from '../../components';
-import {colors, getData, storeData, useForm} from '../../utils';
-import {Firebase} from '../../config';
 import {showMessage} from 'react-native-flash-message';
+import {Button, Gap, Header, Input, Loading} from '../../components';
+import {Firebase} from '../../config';
+import {colors, storeData, useForm} from '../../utils';
 
 export default function Register({navigation}) {
   //initial firebase
