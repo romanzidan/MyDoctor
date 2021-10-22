a58afd0300b02049a499df86bc72b15b82bc3c6f
rzkzx <romanzidanramadhan@gmail.com>
2021-10-23 06:38:55 +0800

    Install Redux & React Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './router';
 import FlashMessage from 'react-native-flash-message';
+import {Loading} from './components';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
-    </>
+      {loading && <Loading />}
+    </Provider>
   );
 }
